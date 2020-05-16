b2e35e4267af9c8091356843809059f39297c471
piotr <piotr@devopswizards.com>
2020-05-16 13:53:09 +0200

    Update xcode installation process

--- init.sh
+++ init.sh
@@ -1,7 +1,16 @@
 #!/usr/bin/env bash
 
 # Install XCode
-xcode-select --install
+if softwareupdate --history | grep --silent "Command Line Tools"; then
+    echo 'Command-line tools already installed.'
+else
+    in_progress_file=/tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress
+    touch ${in_progress_file}
+    for software in `softwareupdate --list | grep 'Command Line Tools' | grep Label` ; do
+        softwareupdate --verbose --install "${software}" || echo "Installation of ${software} failed." 1>&2 && rm ${in_progress_file} && exit 1;
+    done
+    rm ${in_progress_file}
+fi
 
 # Install homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
